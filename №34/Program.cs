eb48eae8a7534c4ec83028b2b40e6fd49c07bfa0
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-20 02:21:03 +0300

    Решили №34 (нахождение количества четных чисел в массиве)

---
+++ №34/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine ("Введите количество элементов Вашего массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] ar = new int [N];
+for (int i = 0; i < N; i++)
+{
+    ar[i] = new Random().Next(100, 1000);
+}
+Console.Write("Вот произвольные трехзначные числа из Вашего массива: ");
+for (int i = 0; i < N; i++)
+{
+   Console.Write(ar[i] + " "); 
+}
+int count = 0;
+int j = 0;
+while (j<ar.Length)
+{
+    if ( ar[j] % 2 == 0)
+    {
+        count++;
+        j++;
+    }
+    else
+    {
+        j++;
+    }
+}
+Console.Write("Количество четных чисел в Вашем массиве = ");
+Console.WriteLine(count);
